c0a694bf3a33acbd9b4c9b24ba7b62cef29c8131
Jean-Luc Bibaud <jean-luc@bibaud.com>
2026-09-24 12:41:20 +0200

    Cleaned up tests to all use Mod which can take mat and oldmat

--- src/mat_tests.erl
+++ src/mat_tests.erl
@@ -2,200 +2,247 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+%% Runs the same matrix test cases against every matrix backend under
+%% test. Each `..._test_/0` is an eunit generator returning one
+%% {BackendName, Fun} per backend (also understood by run_tests.erl).
+
 % to print matrix     io:format(user, "~20p~n", [numerl:mtfli(P3)]),
 
--define(assert_equal(A,B), ?assert(mat:'=='(A,B))).
+-define(BACKENDS, [mat, oldmat]).
 
 
-tr_test() ->
-    M1 = mat:matrix([[1]]),
-    ?assertEqual(M1, mat:tr(M1)),
+tr_test_() -> each_backend(fun tr/1).
 
-    M2 = mat:matrix([[1,2], [3,4]]),
-    ?assertEqual(mat:matrix([[1,3], [2,4]]), mat:tr(M2)),
+tr(Mod) ->
+    M1 = mk(Mod, [[1]]),
+    ?assertEqual(M1, Mod:tr(M1)),
 
-    M3 = mat:matrix([[1,2,3], [4,5,6], [7,8,9]]),
-    M3t = mat:matrix([[1,4,7], [2,5,8], [3,6,9]]),
-    ?assertEqual(M3t, mat:tr(M3)),
+    M2 = mk(Mod, [[1,2], [3,4]]),
+    ?assertEqual(mk(Mod, [[1,3], [2,4]]), Mod:tr(M2)),
 
-    M12 = mat:matrix([[1,2]]),
-    ?assertEqual(mat:matrix([[1], [2]]), mat:tr(M12)),
+    M3 = mk(Mod, [[1,2,3], [4,5,6], [7,8,9]]),
+    M3t = mk(Mod, [[1,4,7], [2,5,8], [3,6,9]]),
+    ?assertEqual(M3t, Mod:tr(M3)),
 
-    M21 = mat:matrix([[1], [2]]),
-    ?assertEqual(mat:matrix([[1,2]]), mat:tr(M21)),
+    M12 = mk(Mod, [[1,2]]),
+    ?assertEqual(mk(Mod, [[1], [2]]), Mod:tr(M12)),
 
-    M23 = mat:matrix([[1,2,3], [4,5,6]]),
-    M23t = mat:matrix([[1,4], [2,5], [3,6]]),
-    ?assertEqual(M23t, mat:tr(M23)),
+    M21 = mk(Mod, [[1], [2]]),
+    ?assertEqual(mk(Mod, [[1,2]]), Mod:tr(M21)),
 
-    M32 = mat:matrix([[1,2], [3,4], [5,6]]),
-    M32t = mat:matrix([[1,3,5], [2,4,6]]),
-    ?assertEqual(M32t, mat:tr(M32)).
+    M23 = mk(Mod, [[1,2,3], [4,5,6]]),
+    M23t = mk(Mod, [[1,4], [2,5], [3,6]]),
+    ?assertEqual(M23t, Mod:tr(M23)),
 
+    M32 = mk(Mod, [[1,2], [3,4], [5,6]]),
+    M32t = mk(Mod, [[1,3,5], [2,4,6]]),
+    ?assertEqual(M32t, Mod:tr(M32)).
 
-'+_test'() ->
-    S1 = mat:'+'(mat:matrix([[1]]), mat:matrix([[1]])),
-    ?assertEqual(mat:matrix([[2]]), S1),
 
-    S2 = mat:'+'(mat:matrix([[1,2], [3,4]]), mat:matrix([[5,6], [7,8]])),
-    ?assertEqual(mat:matrix([[6,8], [10,12]]), S2),
+'+_test_'() -> each_backend(fun plus/1).
 
-    S3 = mat:'+'(mat:matrix([[1], [2]]), mat:matrix([[3], [4]])),
-    ?assertEqual(mat:matrix([[4], [6]]), S3).
+plus(Mod) ->
+    S1 = Mod:'+'(mk(Mod, [[1]]), mk(Mod, [[1]])),
+    ?assertEqual(mk(Mod, [[2]]), S1),
 
+    S2 = Mod:'+'(mk(Mod, [[1,2], [3,4]]), mk(Mod, [[5,6], [7,8]])),
+    ?assertEqual(mk(Mod, [[6,8], [10,12]]), S2),
 
-'-_test'() ->
-    S1 = mat:'-'(mat:matrix([[1]]), mat:matrix([[1]])),
-    ?assertEqual(mat:matrix([[0]]), S1),
+    S3 = Mod:'+'(mk(Mod, [[1], [2]]), mk(Mod, [[3], [4]])),
+    ?assertEqual(mk(Mod, [[4], [6]]), S3).
 
-    S2 = mat:'-'(mat:matrix([[1,2], [3,4]]), mat:matrix([[5,6], [7,8]])),
-    ?assertEqual(mat:matrix([[-4,-4], [-4,-4]]), S2),
 
-    S3 = mat:'-'(mat:matrix([[1], [2]]), mat:matrix([[3], [4]])),
-    ?assertEqual(mat:matrix([[-2], [-2]]), S3).
+'-_test_'() -> each_backend(fun minus/1).
 
+minus(Mod) ->
+    S1 = Mod:'-'(mk(Mod, [[1]]), mk(Mod, [[1]])),
+    ?assertEqual(mk(Mod, [[0]]), S1),
 
-'==_test'() ->
-    M1 = mat:matrix([[1]]),
-    ?assert(mat:'=='(M1, M1)),
+    S2 = Mod:'-'(mk(Mod, [[1,2], [3,4]]), mk(Mod, [[5,6], [7,8]])),
+    ?assertEqual(mk(Mod, [[-4,-4], [-4,-4]]), S2),
 
-    M2 = mat:matrix([[1,2], [3,4]]),
-    ?assert(mat:'=='(M2, M2)),
+    S3 = Mod:'-'(mk(Mod, [[1], [2]]), mk(Mod, [[3], [4]])),
+    ?assertEqual(mk(Mod, [[-2], [-2]]), S3).
 
-    M3 = mat:matrix([[1,2,3], [4,5,6]]),
-    ?assert(mat:'=='(M3, M3)),
 
-    ?assertNot(mat:'=='(M1, M2)),
-    ?assertNot(mat:'=='(M2, M1)).
+'==_test_'() -> each_backend(fun eq/1).
 
+eq(Mod) ->
+    M1 = mk(Mod, [[1]]),
+    ?assert(Mod:'=='(M1, M1)),
 
-'N*M_test'() ->
-    M0 = mat:matrix([[0,0], [0,0]]),
-    ?assert(mat:'=='(M0, mat:'*'(5, M0))),
+    M2 = mk(Mod, [[1,2], [3,4]]),
+    ?assert(Mod:'=='(M2, M2)),
 
-    M1 = mat:matrix([[1,1], [1,1]]),
-    NM1 = mat:matrix([[3,3], [3,3]]),
-    ?assert(mat:'=='(NM1, mat:'*'(3, M1))),
+    M3 = mk(Mod, [[1,2,3], [4,5,6]]),
+    ?assert(Mod:'=='(M3, M3)),
 
-    M2 = mat:matrix([[1,2,3], [-4,-5,-6], [7, -8, 9]]),
-    NM2 = mat:matrix([[-1,-2,-3], [4,5,6], [-7, 8, -9]]),
-    ?assert(mat:'=='(NM2, mat:'*'(-1, M2))).
+    ?assertNot(Mod:'=='(M1, M2)),
+    ?assertNot(Mod:'=='(M2, M1)).
 
 
-'M*M_test'() ->
-    P1 = mat:'*'(mat:matrix([[1]]), mat:matrix([[2]])),
-    ?assertEqual(mat:matrix([[2]]), P1),
+'N*M_test_'() -> each_backend(fun scalar_mul/1).
 
-    P2 = mat:'*'(mat:matrix([[1,2], [3,4]]), mat:matrix([[5,6], [7,8]])),
-    ?assertEqual(mat:matrix([[19,22], [43,50]]), P2),
+scalar_mul(Mod) ->
+    M0 = mk(Mod, [[0,0], [0,0]]),
+    ?assert(Mod:'=='(M0, Mod:'*'(5, M0))),
 
-    P3 = mat:'*'(mat:matrix([[1,2], [3,4]]), mat:matrix([[5], [6]])),
-    ?assertEqual(mat:matrix([[17], [39]]), P3),
+    M1 = mk(Mod, [[1,1], [1,1]]),
+    NM1 = mk(Mod, [[3,3], [3,3]]),
+    ?assert(Mod:'=='(NM1, Mod:'*'(3, M1))),
 
-    P4 = mat:'*'(mat:matrix([[5,6]]), mat:matrix([[1,2], [3,4]])),
-    ?assertEqual(mat:matrix([[23,34]]), P4),
+    M2 = mk(Mod, [[1,2,3], [-4,-5,-6], [7, -8, 9]]),
+    NM2 = mk(Mod, [[-1,-2,-3], [4,5,6], [-7, 8, -9]]),
+    ?assert(Mod:'=='(NM2, Mod:'*'(-1, M2))).
 
-    P5 = mat:'*'(mat:matrix([[1,2]]), mat:matrix([[3], [4]])),
-    ?assertEqual(mat:matrix([[11]]), P5),
 
-    P6 = mat:'*'(mat:matrix([[1], [2]]), mat:matrix([[3,4]])),
-    ?assertEqual(mat:matrix([[3,4], [6,8]]), P6).
+'M*M_test_'() -> each_backend(fun mat_mul/1).
 
+mat_mul(Mod) ->
+    P1 = Mod:'*'(mk(Mod, [[1]]), mk(Mod, [[2]])),
+    ?assertEqual(mk(Mod, [[2]]), P1),
 
-'*´_test'() ->
-    P1 = mat:'*´'(mat:matrix([[1]]), mat:matrix([[2]])),
-    ?assertEqual(mat:matrix([[2]]), P1),
+    P2 = Mod:'*'(mk(Mod, [[1,2], [3,4]]), mk(Mod, [[5,6], [7,8]])),
+    ?assertEqual(mk(Mod, [[19,22], [43,50]]), P2),
 
-    P2 = mat:'*´'(mat:matrix([[1,2], [3,4]]), mat:matrix([[5,7], [6,8]])),
-    ?assertEqual(mat:matrix([[19,22], [43,50]]), P2),
+    P3 = Mod:'*'(mk(Mod, [[1,2], [3,4]]), mk(Mod, [[5], [6]])),
+    ?assertEqual(mk(Mod, [[17], [39]]), P3),
 
-    P3 = mat:'*´'(mat:matrix([[1,2], [3,4]]), mat:matrix([[5,6]])),
-    ?assertEqual(mat:matrix([[17], [39]]), P3),
+    P4 = Mod:'*'(mk(Mod, [[5,6]]), mk(Mod, [[1,2], [3,4]])),
+    ?assertEqual(mk(Mod, [[23,34]]), P4),
 
-    P4 = mat:'*´'(mat:matrix([[5,6]]), mat:matrix([[1,3], [2,4]])),
-    ?assertEqual(mat:matrix([[23,34]]), P4),
+    P5 = Mod:'*'(mk(Mod, [[1,2]]), mk(Mod, [[3], [4]])),
+    ?assertEqual(mk(Mod, [[11]]), P5),
 
-    P5 = mat:'*´'(mat:matrix([[1,2]]), mat:matrix([[3,4]])),
-    ?assertEqual(mat:matrix([[11]]), P5),
+    P6 = Mod:'*'(mk(Mod, [[1], [2]]), mk(Mod, [[3,4]])),
+    ?assertEqual(mk(Mod, [[3,4], [6,8]]), P6).
 
-    P6 = mat:'*´'(mat:matrix([[1], [2]]), mat:matrix([[3], [4]])),
-    ?assertEqual(mat:matrix([[3,4], [6,8]]), P6).
 
+'*´_test_'() -> each_backend(fun mat_mul_tr/1).
 
-row_test() ->
-    M1 = mat:matrix([[1]]),
-    ?assertEqual(mat:matrix([[1]]), mat:row(1, M1)),
+mat_mul_tr(Mod) ->
+    P1 = Mod:'*´'(mk(Mod, [[1]]), mk(Mod, [[2]])),
+    ?assertEqual(mk(Mod, [[2]]), P1),
 
-    M2 = mat:matrix([[1, 2], [3, 4]]),
-    ?assertEqual(mat:matrix([[1,2]]), mat:row(1, M2)),
-    ?assertEqual(mat:matrix([[3,4]]), mat:row(2, M2)).
+    P2 = Mod:'*´'(mk(Mod, [[1,2], [3,4]]), mk(Mod, [[5,7], [6,8]])),
+    ?assertEqual(mk(Mod, [[19,22], [43,50]]), P2),
 
+    P3 = Mod:'*´'(mk(Mod, [[1,2], [3,4]]), mk(Mod, [[5,6]])),
+    ?assertEqual(mk(Mod, [[17], [39]]), P3),
 
-col_test() ->
-    M1 = mat:matrix([[1]]),
-    ?assertEqual(M1, mat:col(1, M1)),
+    P4 = Mod:'*´'(mk(Mod, [[5,6]]), mk(Mod, [[1,3], [2,4]])),
+    ?assertEqual(mk(Mod, [[23,34]]), P4),
 
-    M2 = mat:matrix([[1, 2], [3, 4]]),
-    ?assertEqual(mat:matrix([[1], [3]]), mat:col(1, M2)),
-    ?assertEqual(mat:matrix([[2], [4]]), mat:col(2, M2)).
+    P5 = Mod:'*´'(mk(Mod, [[1,2]]), mk(Mod, [[3,4]])),
+    ?assertEqual(mk(Mod, [[11]]), P5),
 
+    P6 = Mod:'*´'(mk(Mod, [[1], [2]]), mk(Mod, [[3], [4]])),
+    ?assertEqual(mk(Mod, [[3,4], [6,8]]), P6).
 
-get_test() ->
-    M = mat:matrix([[1, 2], [3, 4]]),
-    ?assertEqual(1.0, mat:get(1, 1, M)),
-    ?assertEqual(2.0, mat:get(1, 2, M)),
-    ?assertEqual(3.0, mat:get(2, 1, M)),
-    ?assertEqual(4.0, mat:get(2, 2, M)).
 
+row_test_() -> each_backend(fun row/1).
 
-zeros_test() ->
-    Z1 = mat:matrix([[0]]),
-    ?assertEqual(Z1, mat:zeros(1,1)),
+row(Mod) ->
+    M1 = mk(Mod, [[1]]),
+    ?assertEqual(mk(Mod, [[1]]), Mod:row(1, M1)),
 
-    Z2 = mat:matrix([[0,0], [0,0]]),
-    ?assertEqual(Z2, mat:zeros(2,2)),
+    M2 = mk(Mod, [[1, 2], [3, 4]]),
+    ?assertEqual(mk(Mod, [[1,2]]), Mod:row(1, M2)),
+    ?assertEqual(mk(Mod, [[3,4]]), Mod:row(2, M2)).
 
-    Z12 = mat:matrix([[0,0]]),
-    ?assertEqual(Z12, mat:zeros(1,2)),
 
-    Z21 = mat:matrix([[0], [0]]),
-    ?assertEqual(Z21, mat:zeros(2,1)).
+col_test_() -> each_backend(fun col/1).
 
+col(Mod) ->
+    M1 = mk(Mod, [[1]]),
+    ?assertEqual(M1, Mod:col(1, M1)),
 
-eye_test() ->
-    I1 = mat:matrix([[1]]),
-    ?assertEqual(I1, mat:eye(1)),
+    M2 = mk(Mod, [[1, 2], [3, 4]]),
+    ?assertEqual(mk(Mod, [[1], [3]]), Mod:col(1, M2)),
+    ?assertEqual(mk(Mod, [[2], [4]]), Mod:col(2, M2)).
 
-    I2 = mat:matrix([[1,0], [0,1]]),
-    ?assertEqual(I2, mat:eye(2)),
 
-    I3 = mat:matrix([[1,0,0], [0,1,0], [0,0,1]]),
-    ?assertEqual(I3, mat:eye(3)).
+get_test_() -> each_backend(fun get_elem/1).
 
+get_elem(Mod) ->
+    M = mk(Mod, [[1, 2], [3, 4]]),
+    ?assertEqual(num(Mod, 1), Mod:get(1, 1, M)),
+    ?assertEqual(num(Mod, 2), Mod:get(1, 2, M)),
+    ?assertEqual(num(Mod, 3), Mod:get(2, 1, M)),
+    ?assertEqual(num(Mod, 4), Mod:get(2, 2, M)).
 
-diag_test() ->
-    D1 = mat:matrix([[5]]),
-    ?assertEqual(D1, mat:diag([5])),
 
-    D2 = mat:matrix([[1,0,0], [0,1,0], [0,0,1]]),
-    ?assertEqual(D2, mat:diag([1,1,1])),
+zeros_test_() -> each_backend(fun zeros/1).
 
-    D3 = mat:matrix([[7,0,0], [0,8,0], [0,0,-2]]),
-    ?assertEqual(D3, mat:diag([7,8,-2])).
+zeros(Mod) ->
+    Z1 = mk(Mod, [[0]]),
+    ?assertEqual(Z1, Mod:zeros(1,1)),
 
+    Z2 = mk(Mod, [[0,0], [0,0]]),
+    ?assertEqual(Z2, Mod:zeros(2,2)),
 
-inv_test() ->
-    M1 = mat:matrix([[1]]),
-    ?assert(mat:'=='(M1, mat:inv(M1))),
+    Z12 = mk(Mod, [[0,0]]),
+    ?assertEqual(Z12, Mod:zeros(1,2)),
 
-    I2 = mat:eye(2),
-    M2 = mat:matrix([[1,2], [3,4]]),
-    ?assert(mat:'=='(I2, mat:'*'(M2, mat:inv(M2)))),
+    Z21 = mk(Mod, [[0], [0]]),
+    ?assertEqual(Z21, Mod:zeros(2,1)).
 
-    I3 = mat:eye(3),
-    M3 = mat:matrix([[2,-1,0], [-1,2,-1], [0,-1,2]]),
-    ?assert(mat:'=='(I3, mat:'*'(M3, mat:inv(M3)))),
 
-    I4 = mat:eye(4),
-    M4 = mat:matrix([[1,1,1,0], [0,3,1,2], [2,3,1,0], [1,0,2,1]]),
-    ?assert(mat:'=='(I4, mat:'*'(M4, mat:inv(M4)))).
+eye_test_() -> each_backend(fun eye/1).
+
+eye(Mod) ->
+    I1 = mk(Mod, [[1]]),
+    ?assertEqual(I1, Mod:eye(1)),
+
+    I2 = mk(Mod, [[1,0], [0,1]]),
+    ?assertEqual(I2, Mod:eye(2)),
+
+    I3 = mk(Mod, [[1,0,0], [0,1,0], [0,0,1]]),
+    ?assertEqual(I3, Mod:eye(3)).
+
+
+diag_test_() -> each_backend(fun diag/1).
+
+diag(Mod) ->
+    D1 = mk(Mod, [[5]]),
+    ?assertEqual(D1, Mod:diag([5])),
+
+    D2 = mk(Mod, [[1,0,0], [0,1,0], [0,0,1]]),
+    ?assertEqual(D2, Mod:diag([1,1,1])),
+
+    D3 = mk(Mod, [[7,0,0], [0,8,0], [0,0,-2]]),
+    ?assertEqual(D3, Mod:diag([7,8,-2])).
+
+
+inv_test_() -> each_backend(fun inv/1).
+
+inv(Mod) ->
+    M1 = mk(Mod, [[1]]),
+    ?assert(Mod:'=='(M1, Mod:inv(M1))),
+
+    I2 = Mod:eye(2),
+    M2 = mk(Mod, [[1,2], [3,4]]),
+    ?assert(Mod:'=='(I2, Mod:'*'(M2, Mod:inv(M2)))),
+
+    I3 = Mod:eye(3),
+    M3 = mk(Mod, [[2,-1,0], [-1,2,-1], [0,-1,2]]),
+    ?assert(Mod:'=='(I3, Mod:'*'(M3, Mod:inv(M3)))),
+
+    I4 = Mod:eye(4),
+    M4 = mk(Mod, [[1,1,1,0], [0,3,1,2], [2,3,1,0], [1,0,2,1]]),
+    ?assert(Mod:'=='(I4, Mod:'*'(M4, Mod:inv(M4)))).
+
+
+%% Helpers
+
+each_backend(TestFun) ->
+    [{atom_to_list(Mod), fun() -> TestFun(Mod) end} || Mod <- ?BACKENDS].
+
+%% oldmat has no matrix/1 constructor: its matrix() type is already a
+%% plain nested list, so construction is the identity for that backend.
+mk(mat, L) -> mat:matrix(L);
+mk(oldmat, L) -> L.
+
+%% mat stores elements as floats, oldmat keeps them as given.
+num(mat, N) -> float(N);
+num(oldmat, N) -> N.
